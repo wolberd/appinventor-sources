0f77653d941ae8140cde974c7432e49af6f9c0ba
niki <niki@mit.edu>
2013-11-20 10:42:00 -0500

    Added exit option

--- appinventor/appengine/src/com/google/appinventor/client/explorer/youngandroid/NewUserGetStarted.java
+++ appinventor/appengine/src/com/google/appinventor/client/explorer/youngandroid/NewUserGetStarted.java
@@ -58,6 +58,10 @@ public class NewUserGetStarted{
 				slides.get(currentMessageIndex).show();
 			}
 		}
+		
+		public void exitTutorial() {
+			slides.get(currentMessageIndex).hide();
+		}
 		  
 	}
 
